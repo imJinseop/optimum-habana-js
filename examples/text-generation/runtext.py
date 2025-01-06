f3e50f9eea57297c12059039e598ef612389f204
imJinseop <jinseop.im@kaist.ac.kr>
2025-01-06 19:08:53 +0900

    end-to-end autorun

---
+++ examples/text-generation/runtext.py
@@ -0,0 +1,64 @@
+import os
+# model_pair = {"gpt2": "gpt2", "bloom": "bigscience/bloom"}
+model_pair = {"gpt2": "gpt2"}
+
+def make_script(model, eval, type):
+
+    script = ""
+    if (type == "fp8"):
+        script += "QUANT_CONFIG=./quantization_config/maxabs_quant.json \\\n"
+    elif (type == "fp8_measure"):
+    # script += "QUANT_CONFIG=./quantization_config/maxabs_quant_e5m2.json \\\n"
+        script += "QUANT_CONFIG=./quantization_config/maxabs_measure.json \\\n"
+    # script += "QUANT_CONFIG=./quantization_config/maxabs_measure_e5m2.json \\\n"
+    elif (type == "bf16"):
+        bf16 = True
+    elif (type == "fp32"):
+        bf16 = False
+    else:
+        assert(0)
+    
+    if (eval):
+        script += "python run_lm_eval.py \\\n"
+        # for run_lm_eval.py
+        script += f"--output_file ./logs/{model}/eval/{type}.json \\\n"
+    else:
+        script += "python run_generation.py \\\n"
+    # script += "--model_name_or_path /model_weights/meta-llama/Llama-3.1-8B-Instruct/ \\\n"
+    script += f"--model_name_or_path {model_pair[model]} \\\n"
+
+    # for run_generation.py
+    # script += "--output_dir ./logs/run_generation/fp8_measure \\\n"
+
+    if (type != "fp32"):
+        script += "--bf16 \\\n"
+
+    if (not eval):
+        script += "--book_source \\\n"
+        script += "--max_input_tokens 100 \\\n"
+        script += "--max_new_tokens 100 \\\n"
+        script += "--batch_size 16 \\\n"
+    
+    script += "--use_kv_cache \\\n"
+    # script += "--reuse_cache \\\n"
+    # script += "--n_iterations 10 \\\n"
+    script += "--use_hpu_graphs \\\n"
+    script += "--use_flash_attention \\\n"
+
+
+
+    script += "--device hpu"
+    if (not eval):
+        script += f" >./logs/{model}/run_generation/{type}.txt 2>&1"
+
+    # print(script)
+    return script
+
+
+types = ["fp8_measure", "fp8"]
+# types = ["fp32", "bf16", "fp8_measure", "fp8"]
+for model in model_pair.keys():
+    for type in types:
+        os.system(make_script(model, False, type=type))
+        os.system(make_script(model, True, type=type))
+        
